8b1aa81f9c3d33a0698408dba42a91303b10dca0
Ciobanu Andy <andyciobanu17@gmail.com>
2021-10-16 16:16:19 +0300

    Cooking aparatus feature added

--- Core/Models/KitchenFoodItem.cs
+++ Core/Models/KitchenFoodItem.cs
@@ -7,6 +7,7 @@ namespace Kitchen.Core.Models
           public Guid OrderId { get; set; }
           public int Id { get; set; }
           public int Complexity { get; set; }
+          public string Name { get; set; }
           public CookingTool CookingTool { get; set; }
           public int PreparationTime { get; set; }
      }
